5f1301d5847c10914e74283a72744759a2a72f5b
jsakshu <96901179+jsakshu@users.noreply.github.com>
2025-07-15 23:49:57 +0530

    Add files via upload

---
+++ sql stored procedure.sql
@@ -0,0 +1,122 @@
+
+
+
+DELIMITER //
+
+CREATE PROCEDURE get_student_info1()
+BEGIN
+    SELECT * FROM Student;
+END //
+
+DELIMITER ;
+
+DELIMITER //
+
+CREATE PROCEDURE get_department_info()
+BEGIN
+    SELECT * FROM Department;
+END //
+
+DELIMITER ;
+
+
+DELIMITER //
+
+CREATE PROCEDURE get_students(IN depid int)
+BEGIN
+	Select s.name,d.dept_name
+    from Student as s 
+    join Department as d
+    on s.dept_id=d.dept_id
+    where s.dept_id=depid;
+END //
+
+DELIMITER ;
+
+DELIMITER //
+
+CREATE PROCEDURE AddStudent (
+    IN p_Roll VARCHAR(10),
+    IN p_Name VARCHAR(100),
+    IN p_Email VARCHAR(100),
+    IN p_DeptId INT
+)
+BEGIN
+    INSERT INTO Student (student_roll, name, email, dept_id)
+    VALUES (p_Roll, p_Name, p_Email, p_DeptId);
+END //
+
+DELIMITER ;
+
+
+DELIMITER //
+
+CREATE PROCEDURE GetStudentEmail (
+    IN roll VARCHAR(10),
+    OUT student_email VARCHAR(100)
+)
+BEGIN
+    SELECT email INTO student_email
+    FROM Student
+    WHERE student_roll = roll;
+END //
+
+DELIMITER ;
+
+DELIMITER //
+
+CREATE PROCEDURE GetTotalStudents(
+    OUT total INT
+)
+BEGIN
+    SELECT COUNT(*) INTO total FROM Student;
+END //
+
+DELIMITER ;
+
+DELIMITER //
+
+CREATE PROCEDURE GetStudentCountByDept(
+    IN deptName VARCHAR(50),
+    OUT studentCount INT
+)
+BEGIN
+    SELECT COUNT(*) INTO studentCount
+    FROM Student S
+    JOIN Department D ON S.dept_id = D.dept_id
+    WHERE D.dept_name = deptName;
+END //
+
+DELIMITER ;
+
+
+
+DELIMITER //
+
+CREATE PROCEDURE UpdateDept (
+    INOUT roll VARCHAR(10),
+    IN new_dept INT
+)
+BEGIN
+    UPDATE Student SET dept_id = new_dept
+    WHERE student_roll = roll;
+END //
+
+DELIMITER ;
+
+
+DELIMITER $$
+
+CREATE PROCEDURE count_students_inout(
+    INOUT total INT
+)
+BEGIN
+    DECLARE temp INT;
+    SELECT COUNT(*) INTO temp FROM student;
+    SET total = total + temp;
+END $$
+
+DELIMITER ;
+
+
+
